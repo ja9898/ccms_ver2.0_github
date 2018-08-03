ea1f9e6929675f679f661ed8678d0dc32bc97162
Junaid <junaid9898@yahoo.com>
2018-08-03 16:45:41 +0500

    'Delete'

--- assign_delete.php
+++
@@ -1,7 +0,0 @@
-<? 
-include('config.php'); 
-include('include/header.php');
-$id = (int) $_GET['id']; 
-mysql_query("DELETE FROM `campus_assignment` WHERE `id` = '$id' ") ; 
-getMessages('delete');
-include('include/footer.php');?>
--- user_type_delete.php
+++
@@ -1,9 +0,0 @@
-<? 
-include('config.php'); 
-include('include/header.php');
-$id = (int) $_GET['id']; 
-mysql_query("DELETE FROM `campus_usertype` WHERE `id` = '$id' ") ; 
-getMessages('delete','user_type_list.php');
-?> 
-
-<?php include('include/footer.php');?>
